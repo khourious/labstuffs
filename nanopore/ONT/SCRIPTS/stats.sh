1251b95a3396d760f13b1e85c5c5538f3b9bf23d
lpmor22 <laisepaixao@live.com>
2021-01-24 12:45:52 -0300

    PATH change

--- nanopore/ONT/SCRIPTS/stats.sh
+++ nanopore/ONT/SCRIPTS/stats.sh
@@ -8,12 +8,12 @@ ref=`echo "$pathref" | sed -e 's/\/.*//g'`
 
 echo -n "$sample""@" | tr '@' '\t' >> "$library".stats.txt
 
-$HOME/CADDE/SCRIPTS/nb_reads_mapped.sh "$sample".sorted.bam | awk '{printf $1"@"}' | tr '@' '\t' >> "$library".stats.txt
+$HOME/ONT/SCRIPTS/nb_reads_mapped.sh "$sample".sorted.bam | awk '{printf $1"@"}' | tr '@' '\t' >> "$library".stats.txt
 
-$HOME/CADDE/SCRIPTS/depth.sh "$sample".sorted.bam | awk '{printf $1"@"}' | tr '@' '\t' >> "$library".stats.txt
+$HOME/ONT/SCRIPTS/depth.sh "$sample".sorted.bam | awk '{printf $1"@"}' | tr '@' '\t' >> "$library".stats.txt
 
-$HOME/CADDE/SCRIPTS/bases_covered_x.sh "$sample".sorted.bam 10 | awk '{printf $1"@"}' | tr '@' '\t' >> "$library".stats.txt
+$HOME/ONT/SCRIPTS/bases_covered_x.sh "$sample".sorted.bam 10 | awk '{printf $1"@"}' | tr '@' '\t' >> "$library".stats.txt
 
-$HOME/CADDE/SCRIPTS/bases_covered_x.sh "$sample".sorted.bam 25 | awk '{printf $1"@"}' | tr '@' '\t' >> "$library".stats.txt
+$HOME/ONT/SCRIPTS/bases_covered_x.sh "$sample".sorted.bam 25 | awk '{printf $1"@"}' | tr '@' '\t' >> "$library".stats.txt
 
-$HOME/CADDE/SCRIPTS/coverage.sh $HOME/CADDE/PRIMER_SCHEMES/"$pathref"/"$ref".reference.fasta "$sample".consensus.fasta | awk '{print $2}' >> "$library".stats.txt
+$HOME/ONT/SCRIPTS/coverage.sh $HOME/ONT/PRIMER_SCHEMES/"$pathref"/"$ref".reference.fasta "$sample".consensus.fasta | awk '{print $2}' >> "$library".stats.txt
