4c82fe6be94b29296db7dcc3d2be41730479a086
lpmor22 <laisepaixao@live.com>
2021-03-16 17:06:20 -0300

    Update metagenomic_v2.sh

--- etc/metagenomic_v2.sh
+++ etc/metagenomic_v2.sh
@@ -16,28 +16,15 @@ THREADS="12"
 NUMCALLERS="60" #NVIDIAGeForceRTX2060=60; NVIDIAGeForceRTX2080=46
 GPUPERDEVICE="32" #NVIDIAGeForceRTX2060=32; NVIDIAGeForceRTX2080=64
 
-RAWDIR="/media/lpmor22/2019-12-19/DENV_Seq_Data/DENV_FTA_1/DENV_Run1_data"
-HACBASECALLDIR="/media/lpmor22/2019-12-19/DENV_Seq_Data/DENV_FTA_1/HAC_BASECALL"
-DEMUXDIR="/media/lpmor22/2019-12-19/DENV_Seq_Data/DENV_FTA_1/DEMUX"
-CONCATENATED="/media/lpmor22/2019-12-19/DENV_Seq_Data/DENV_FTA_1/DEMUX_concatenated"
-READLEVELDIR="/media/lpmor22/2019-12-19/DENV_Seq_Data/DENV_FTA_1/READ_level_analysis"
-CONTIGLEVELDIR="/media/lpmor22/2019-12-19/DENV_Seq_Data/DENV_FTA_1/CONTIG_level_analysis"
-RUNNAME="DENV_FTA_1"
+RAWDIR="/media/lpmor22/2019-12-19/DENV_Seq_Data/DENV_FTA_2/DENV_FTA2"
+HACBASECALLDIR="/media/lpmor22/2019-12-19/DENV_Seq_Data/DENV_FTA_2/HAC_BASECALL"
+DEMUXDIR="/media/lpmor22/2019-12-19/DENV_Seq_Data/DENV_FTA_2/DEMUX"
+CONCATENATED="/media/lpmor22/2019-12-19/DENV_Seq_Data/DENV_FTA_2/DEMUX_concatenated"
+READLEVELDIR="/media/lpmor22/2019-12-19/DENV_Seq_Data/DENV_FTA_2/READ_level_analysis"
+CONTIGLEVELDIR="/media/lpmor22/2019-12-19/DENV_Seq_Data/DENV_FTA_2/CONTIG_level_analysis"
+RUNNAME="DENV_FTA_2"
 
-SAMPLEID1="barcode01"
-SAMPLEID2="barcode02"
-SAMPLEID3="barcode03"
-SAMPLEID4="barcode04"
-SAMPLEID5="barcode05"
-SAMPLEID6="barcode06"
-SAMPLEID7="barcode07"
-SAMPLEID8="barcode08"
-SAMPLEID9="barcode09"
-SAMPLEID10="barcode10"
-SAMPLEID11="barcode11"
-SAMPLEID12="barcode12"
-
-REFSEQ="/media/lpmor22/2019-12-19/DENV_Seq_Data/DENV_FTA_1/GCF_000001405.39_GRCh38.p13_genomic.fna.gz"
+REFSEQ="/media/lpmor22/2019-12-19/DENV_Seq_Data/DENV_FTA_2/GCF_000001405.39_GRCh38.p13_genomic.fna.gz"
 # ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/001/405/GCF_000001405.39_GRCh38.p13
 
 guppy_basecaller -r -i ${RAWDIR} -s ${HACBASECALLDIR} -c ${CONFIG} --arrangements_files ${ARRANGEMENTS} --qscore_filtering --min_qscore ${MINSCORE} -x auto --gpu_runners_per_device ${GPUPERDEVICE} --num_callers ${NUMCALLERS} --verbose_logs
@@ -49,20 +36,10 @@ pycoQC -q -f ${HACBASECALLDIR}/sequencing_summary.txt -b ${DEMUXDIR}/barcoding_s
 
 [ ! -d "${CONCATENATED}" ] && mkdir -vp ${CONCATENATED}
 [ ! -d "${READLEVELDIR}" ] && mkdir -vp ${READLEVELDIR}
-[ ! -d "${CONTIGLEVELDIR}" ] && mkdir -vp ${CONTIGLEVELDIR}
 
-[ -d "${DEMUXDIR}/barcode01" ] && cat ${DEMUXDIR}/barcode01/*.fastq > ${CONCATENATED}/${SAMPLEID1}.fastq
-[ -d "${DEMUXDIR}/barcode02" ] && cat ${DEMUXDIR}/barcode02/*.fastq > ${CONCATENATED}/${SAMPLEID2}.fastq
-[ -d "${DEMUXDIR}/barcode03" ] && cat ${DEMUXDIR}/barcode03/*.fastq > ${CONCATENATED}/${SAMPLEID3}.fastq
-[ -d "${DEMUXDIR}/barcode04" ] && cat ${DEMUXDIR}/barcode04/*.fastq > ${CONCATENATED}/${SAMPLEID4}.fastq
-[ -d "${DEMUXDIR}/barcode05" ] && cat ${DEMUXDIR}/barcode05/*.fastq > ${CONCATENATED}/${SAMPLEID5}.fastq
-[ -d "${DEMUXDIR}/barcode06" ] && cat ${DEMUXDIR}/barcode06/*.fastq > ${CONCATENATED}/${SAMPLEID6}.fastq
-[ -d "${DEMUXDIR}/barcode07" ] && cat ${DEMUXDIR}/barcode07/*.fastq > ${CONCATENATED}/${SAMPLEID7}.fastq
-[ -d "${DEMUXDIR}/barcode08" ] && cat ${DEMUXDIR}/barcode08/*.fastq > ${CONCATENATED}/${SAMPLEID8}.fastq
-[ -d "${DEMUXDIR}/barcode09" ] && cat ${DEMUXDIR}/barcode09/*.fastq > ${CONCATENATED}/${SAMPLEID9}.fastq
-[ -d "${DEMUXDIR}/barcode10" ] && cat ${DEMUXDIR}/barcode10/*.fastq > ${CONCATENATED}/${SAMPLEID10}.fastq
-[ -d "${DEMUXDIR}/barcode11" ] && cat ${DEMUXDIR}/barcode11/*.fastq > ${CONCATENATED}/${SAMPLEID11}.fastq
-[ -d "${DEMUXDIR}/barcode12" ] && cat ${DEMUXDIR}/barcode12/*.fastq > ${CONCATENATED}/${SAMPLEID12}.fastq
+for i in $(find ${DEMUXDIR} -mindepth 1 -type d -name "barcode*" -exec basename {} \; | sort); do
+	[ -d "${DEMUXDIR}/${i}" ] && cat ${DEMUXDIR}/${i}/*.fastq > ${CONCATENATED}/${i}.fastq;
+done
 
 for i in $(find ${CONCATENATED} -type f -name "*.fastq" | while read o; do basename $o | cut -d. -f1; done | sort | uniq); do
 	source activate minimap2
