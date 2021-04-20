5cd383382a8a39193adf27c61cf3c792a0e28c39
lpmor22 <laisepaixao@live.com>
2021-04-20 05:58:35 -0300

    Update stats.sh

--- WGS/SCRIPTS/stats.sh
+++ WGS/SCRIPTS/stats.sh
@@ -8,12 +8,12 @@ ref=`echo "$pathref" | sed -e 's/\/.*//g'`
 
 echo -n "$sample""@" | tr '@' '\t' >> "$library".stats.txt
 
-$HOME/ONT/SCRIPTS/nb_reads_mapped.sh "$sample".sorted.bam | awk '{printf $1"@"}' | tr '@' '\t' >> "$library".stats.txt
+nb_reads_mapped.sh "$sample".trimmed.sorted.bam | awk '{printf $1"@"}' | tr '@' '\t' >> "$library".stats.txt
 
-$HOME/ONT/SCRIPTS/depth.sh "$sample".sorted.bam | awk '{printf $1"@"}' | tr '@' '\t' >> "$library".stats.txt
+depth.sh "$sample".trimmed.sorted.bam | awk '{printf $1"@"}' | tr '@' '\t' >> "$library".stats.txt
 
-$HOME/ONT/SCRIPTS/bases_covered_x.sh "$sample".sorted.bam 10 | awk '{printf $1"@"}' | tr '@' '\t' >> "$library".stats.txt
+bases_covered_x.sh "$sample".trimmed.sorted.bam 10 | awk '{printf $1"@"}' | tr '@' '\t' >> "$library".stats.txt
 
-$HOME/ONT/SCRIPTS/bases_covered_x.sh "$sample".sorted.bam 25 | awk '{printf $1"@"}' | tr '@' '\t' >> "$library".stats.txt
+bases_covered_x.sh "$sample".trimmed.sorted.bam 25 | awk '{printf $1"@"}' | tr '@' '\t' >> "$library".stats.txt
 
-$HOME/ONT/SCRIPTS/coverage.sh $HOME/ONT/PRIMER_SCHEMES/"$pathref"/"$ref".reference.fasta "$sample".consensus.fasta | awk '{print $2}' >> "$library".stats.txt
+coverage.sh "$ref".reference.fasta "$sample".consensus.fa | awk '{print $2}' >> "$library".stats.txt
