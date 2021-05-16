e682b451e3832c0fab8a0c6da6704cd041582d12
lpmor22 <laisepaixao@live.com>
2021-05-16 09:05:42 -0300

    Update linux.sh

--- configs/linux.sh
+++ configs/linux.sh
@@ -20,3 +20,19 @@ ln -s /folder/ name
 
 # start on linux directory
 "startingDirectory": "//wsl$/Ubuntu-20.04/home/lpmor22"
+
+# acaraje
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash Miniconda3-latest-Linux-x86_64.sh -bfp miniconda3
+rm Miniconda3-latest-Linux-x86_64.sh
+export PATH=$HOME/miniconda3/bin:/usr/local/share/rsi/idl/bin:$PATH
+conda install -y -c conda-forge mamba
+mamba update -y -n base conda
+mamba install -y -c conda-forge dos2unix git htop zsh
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
+echo 'export PATH=$HOME/miniconda3/bin:/usr/local/share/rsi/idl/bin:$PATH' >> $HOME/.zshrc
+rm -rf $HOME/.bashrc && touch $HOME/.bashrc
+echo 'export PATH=$HOME/miniconda3/bin:/usr/local/share/rsi/idl/bin:$PATH' >> $HOME/.bashrc
+echo 'zsh' >> $HOME/.bashrc
