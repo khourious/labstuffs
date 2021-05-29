e1c3d3b08a62f1740bef59107c6971afe9e417e7
lpmor22 <laisepaixao@live.com>
2021-05-29 19:21:42 -0300

    Update linux.sh

--- configs/linux.sh
+++ configs/linux.sh
@@ -1,19 +1,23 @@
-#i7-9750H:12_threads|GeForce_2060:1920_cudaCores
-#i7-9750H:12_threads|GeForce_2080:2944_cudaCores
+# 2021-05-29
+# i7-9750H:12_threads|GeForce_2060:1920_cudaCores
+# i7-9750H:12_threads|GeForce_2080:2944_cudaCores
 
 # add swap memory
 sudo swapon --show && sudo fallocate -l 32G /swapfile && sudo chmod 600 /swapfile && sudo mkswap /swapfile && sudo swapon /swapfile && sudo swapon --show && echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+
+# install prioritary packages
+sudo apt-get install -y autoconf build-essential curl dos2unix exfat-fuse git g++-10 gcc-10 htop pkg-config python3-pip r-base-core sshpass
+
+libcanberra-gtk-module libcanberra-gtk3-module libdeflate-dev libfreetype6-dev libgl1-mesa-dev libglew-dev libglm-dev libsdl2-dev libsdl2-image-dev libtool mesa-utils ocl-icd-opencl-dev ocl-icd-libopencl1 openjdk-14-jdk openjdk-14-jre
 
 # zsh
 sudo apt-get install -y zsh
 sudo chsh --shell /bin/zsh lpmor22
 sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
-wsl.exe --shutdown
+wsl.exe --shutdown # WSL
+reboot # linux
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
-
-# install prioritary packages
-sudo apt-get install -y autoconf build-essential dos2unix exfat-fuse g++-10 gcc-10 git libcanberra-gtk-module libcanberra-gtk3-module libdeflate-dev libfreetype6-dev libgl1-mesa-dev libglew-dev libglm-dev libsdl2-dev libsdl2-image-dev libtool mesa-utils ocl-icd-opencl-dev ocl-icd-libopencl1 openjdk-14-jdk openjdk-14-jre pkg-config python3-pip r-base-core sshpass
 
 # create symbolic links
 ln -s /folder/ name
