87b7ab6c66eb42f617d58759c29992597030a51b
Laise de Moraes <laisepaixao@live.com>
2021-07-04 16:50:01 -0300

    Delete Guppy_commands.bash

--- etc/Guppy_commands.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#basecalling data from .fast5 files with guppy
-
-guppy_basecaller --input_path /path/to/fast5 --save_path /path/for/output --flowcell FLO-MIN106 --kit SQK-LSK109 --qscore_filtering --min_qscore 8
-
-#demultiplexing, trimming, and filtering reads with guppy_barcoder
-
-guppy_barcoder --input_path /path/to/fastq --save_path /path/for/output --require_barcode_both_ends --trim_barcodes --num_extra_bases_trim 18 --arrangements_files "barcode_arrs_nb12.cfg barcode_arrs_nb24.cfg"
