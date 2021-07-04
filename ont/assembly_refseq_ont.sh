20653f6eb4efd25eee046c4ca79e41b69d2cf255
Laise de Moraes <laisepaixao@live.com>
2021-07-04 16:52:15 -0300

    Update assembly_refseq_ont.sh

--- ont/assembly_refseq_ont.sh
+++ ont/assembly_refseq_ont.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# author: Laise de Moraes <laisepaixao@live.com>
-# institution: Oswaldo Cruz Foundation, Gonçalo Moniz Institute, Bahia, Brazil
-# URL: https://lpmor22.github.io
-# date: 30 MAY 2021
-
 THREADS="$(lscpu | grep 'CPU(s):' | awk '{print $2}' | sed -n '1p')"
 
 RAWDIR="/path/directory"
@@ -16,16 +11,29 @@ REFSEQ=""
 PLOIDY="" #2
 
 source activate minimap2
+
 minimap2 -t $THREADS -ax map-ont $REFSEQ $SAMPLE | samtools sort -@ $THREADS -o $SAMPLEID.sorted.bam -
+
 samtools view -@ $THREADS -h -F 4 -b $SAMPLEID.sorted.bam > $SAMPLEID.sorted.mapped.bam
+
 samtools view -@ $THREADS -bS -f 4 $SAMPLEID.sorted.bam > $SAMPLEID.sorted.unmapped.bam
+
 samtools index -@ $THREADS $SAMPLEID.sorted.mapped.bam
+
 source activate nanopolish
+
 nanopolish index -d $RAWDIR -s $HACDEMUXDIR/sequencing_summary.txt $SAMPLE
+
 mkdir nanopolish
+
 nanopolish_makerange.py $REFSEQ --overlap-length -1 | parallel -P 1 nanopolish variants -t $THREADS -r $SAMPLE -b $SAMPLEID.sorted.mapped.bam -o nanopolish/$SAMPLEID.{#}.vcf -g $REFSEQ -w {1} -p $PLOIDY -v
+
 ls nanopolish/*.vcf > $SAMPLEID.vcflist.txt
+
 nanopolish vcf2fasta --skip-checks -g $REFSEQ -f $SAMPLEID.vcflist.txt > $SAMPLEID.consensus.fasta
+
 source activate minimap2
+
 samtools coverage $SAMPLEID.sorted.mapped.bam > $SAMPLEID.sorted.mapped.coverage
+
 samtools coverage $SAMPLEID.sorted.mapped.bam -A > $SAMPLEID.sorted.mapped.histogram.coverage
