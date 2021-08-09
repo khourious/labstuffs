402b9c3e7efd4271cbd6d26274697581d6ef9b8d
Laise de Moraes <laisepaixao@live.com>
2021-08-08 22:42:43 -0300

    add SAMPLESHEET var;  LEVEL var corrections;  add featurecounts analysis

--- rnaseq/rnaseq_arbovirus_zkproject.sh
+++ rnaseq/rnaseq_arbovirus_zkproject.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-while getopts ":i:l:t:" opt; do
+while getopts ":i:l:s:t:" opt; do
         case $opt in
                 i ) INPUT="$OPTARG";;
                 l ) LEVEL="$OPTARG";;
+                s ) SAMPLESHEET="$OPTARG";;
                 t ) THREADS="$OPTARG";;
         esac
 done
@@ -57,10 +58,12 @@ if [[ "$LEVEL" = "0" ]]; then
         cd
         wget https://ufpr.dl.sourceforge.net/project/subread/subread-"$version"/subread-"$version"-source.tar.gz
         tar -xvf subread-"$version"-source.tar.gz
-        cd subread-"$version"-source/src
+        rm subread-"$version"-source.tar.gz
+        mv subread-"$version"-source subread-source
+        cd subread-source/src
         make -f Makefile.Linux
-        echo 'export PATH=$HOME/subread-"$version"-source/bin:/usr/local/share/rsi/idl/bin:$PATH' >> $HOME/.${MYSHELL}rc
-        export PATH=$HOME/subread-"$version"-source/bin:/usr/local/share/rsi/idl/bin:$PATH
+        echo 'export PATH=$HOME/subread-source/bin:/usr/local/share/rsi/idl/bin:$PATH' >> $HOME/.${MYSHELL}rc
+        export PATH=$HOME/subread-source/bin:/usr/local/share/rsi/idl/bin:$PATH
     else
         featureCounts -v
     fi
@@ -80,8 +83,10 @@ bg() {
         RAWRUNDIR4="$RAWDIR"/FASTQ_Generation_2018-12-13_14_45_41Z-143225097
         RAWRUNDIR5="$RAWDIR"/FASTQ_Generation_2018-12-15_10_26_29Z-143716575
         ANALYSIS="$RAWDIR"/ANALYSIS
-        mkdir "$ANALYSIS" "$ANALYSIS"/ALIGN "$ANALYSIS"/INDEX "$ANALYSIS"/QC_RUN{1..5} "$ANALYSIS"/QC_RUNS_MERGED \
-        "$ANALYSIS"/REFERENCE "$ANALYSIS"/RUN{1..5} "$ANALYSIS"/RUNS_MERGED "$ANALYSIS"/TRIMMED
+        if [[ ! -d "$ANALYSIS" ]]; then
+            mkdir "$ANALYSIS" "$ANALYSIS"/ALIGN "$ANALYSIS"/INDEX "$ANALYSIS"/QC_RUN{1..5} "$ANALYSIS"/QC_RUNS_MERGED \
+            "$ANALYSIS"/REFERENCE "$ANALYSIS"/RUN{1..5} "$ANALYSIS"/RUNS_MERGED "$ANALYSIS"/TRIMMED
+        fi
     fi
 
     if [[ "$LEVEL" == 1 ]]; then
@@ -112,7 +117,8 @@ bg() {
         rm -rf "$ANALYSIS"/RUN{1..5}/*_001.fastq.gz
         for i in $(find "$ANALYSIS" -maxdepth 1 -mindepth 1 -type d -name "RUN*[1-5]" | awk -F/ '{print $NF}'); do
             fastqc -t "$THREADS" "$ANALYSIS"/"$i"/*RUN* -o "$ANALYSIS"/QC_"$i"
-            multiqc -s -i "$i" -b "ArbovirusFiocruzBA-83677594 "$i" merged lanes" -ip --no-data-dir -n "$ANALYSIS"/"$i"_merged_lanes_multiqc_report "$ANALYSIS"/QC_"$i"/*RUN*
+            multiqc -s -i "$i" -b "ArbovirusFiocruzBA-83677594 "$i" merged lanes" -ip --no-data-dir \
+            -n "$ANALYSIS"/"$i"_merged_lanes_multiqc_report "$ANALYSIS"/QC_"$i"/*RUN*
         done
     fi
 
@@ -123,28 +129,56 @@ bg() {
         done
         rm -rf "$ANALYSIS"/RUN{1..5}
         fastqc -t "$THREADS" "$ANALYSIS"/RUNS_MERGED/*.fastq.gz -o "$ANALYSIS"/QC_RUNS_MERGED
-        multiqc -s -i "ArbovirusFiocruzBA-83677594 RUNS_MERGED" -ip --no-data-dir -n "$ANALYSIS"/RUNS_MERGED_multiqc_report "$ANALYSIS"/QC_RUNS_MERGED/*MERGED*
+        multiqc -s -i "ArbovirusFiocruzBA-83677594 RUNS_MERGED" -ip --no-data-dir \
+        -n "$ANALYSIS"/RUNS_MERGED_multiqc_report "$ANALYSIS"/QC_RUNS_MERGED/*MERGED*
     fi
 
     if [[ "$LEVEL" == 4 ]]; then
         for i in $(find "$ANALYSIS"/RUNS_MERGED -type f -name "*.fastq.gz" | awk -F/ '{print $NF}' | awk -F_ '{print $1"_"$2}' | sort -u); do
-            trimmomatic PE -threads "$THREADS" -phred33 "$ANALYSIS"/RUNS_MERGED/"$i"_MERGED_R1.fastq.gz "$ANALYSIS"/RUNS_MERGED/"$i"_MERGED_R2.fastq.gz "$ANALYSIS"/TRIMMED/"$i"_R1_PAIRED.fastq.gz "$ANALYSIS"/TRIMMED/"$i"_R1_UNPAIRED.fastq.gz "$ANALYSIS"/TRIMMED/"$i"_R2_PAIRED.fastq.gz "$ANALYSIS"/TRIMMED/"$i"_R2_UNPAIRED.fastq.gz LEADING:3 TRAILING:3 SLIDINGWINDOW:4:25 MINLEN:36
+            trimmomatic PE -threads "$THREADS" -phred33 \
+            "$ANALYSIS"/RUNS_MERGED/"$i"_MERGED_R1.fastq.gz "$ANALYSIS"/RUNS_MERGED/"$i"_MERGED_R2.fastq.gz \
+            "$ANALYSIS"/TRIMMED/"$i"_R1_PAIRED.fastq.gz "$ANALYSIS"/TRIMMED/"$i"_R1_UNPAIRED.fastq.gz \
+            "$ANALYSIS"/TRIMMED/"$i"_R2_PAIRED.fastq.gz "$ANALYSIS"/TRIMMED/"$i"_R2_UNPAIRED.fastq.gz \
+            LEADING:3 TRAILING:3 SLIDINGWINDOW:4:25 MINLEN:36
         done
     fi
 
     if [[ "$LEVEL" == 5 ]]; then
-        wget http://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_38/GRCh38.p13.genome.fa.gz -q -O "$ANALYSIS"/REFERENCE/GRCh38.p13.genome.fa.gz
-        wget http://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_38/gencode.v38.chr_patch_hapl_scaff.annotation.gtf.gz -q -O "$ANALYSIS"/REFERENCE/gencode.v38.chr_patch_hapl_scaff.annotation.gtf.gz
+        wget http://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_38/GRCh38.p13.genome.fa.gz -q \
+        -O "$ANALYSIS"/REFERENCE/GRCh38.p13.genome.fa.gz
+        wget http://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_38/gencode.v38.chr_patch_hapl_scaff.annotation.gtf.gz -q \
+        -O "$ANALYSIS"/REFERENCE/gencode.v38.chr_patch_hapl_scaff.annotation.gtf.gz
         gunzip "$ANALYSIS"/REFERENCE/GRCh38.p13.genome.fa.gz
         gunzip "$ANALYSIS"/REFERENCE/gencode.v38.chr_patch_hapl_scaff.annotation.gtf.gz
         STAR --runThreadN "$THREADS" --runMode genomeGenerate --genomeDir "$ANALYSIS"/INDEX --genomeFastaFiles "$ANALYSIS"/REFERENCE/GRCh38.p13.genome.fa
         for i in $(find "$ANALYSIS"/TRIMMED -type f -name "*.fastq.gz" | awk -F/ '{print $NF}' | awk -F_ '{print $1"_"$2}' | sort -u); do
-            STAR --runThreadN "$THREADS" --runMode alignReads --genomeDir "$ANALYSIS"/INDEX --sjdbGTFfile "$ANALYSIS"/REFERENCE/gencode.v38.chr_patch_hapl_scaff.annotation.gtf --readFilesIn "$ANALYSIS"/TRIMMED/"$i"_R1_PAIRED.fastq.gz "$ANALYSIS"/TRIMMED/"$i"_R2_PAIRED.fastq.gz --readFilesCommand zcat --outFileNamePrefix "$ANALYSIS"/ALIGN/"$i"_ --outSAMtype BAM Unsorted --outReadsUnmapped Fastx
+            STAR --runThreadN "$THREADS" --runMode alignReads --genomeDir "$ANALYSIS"/INDEX \
+            --sjdbGTFfile "$ANALYSIS"/REFERENCE/gencode.v38.chr_patch_hapl_scaff.annotation.gtf \
+            --readFilesIn "$ANALYSIS"/TRIMMED/"$i"_R1_PAIRED.fastq.gz "$ANALYSIS"/TRIMMED/"$i"_R2_PAIRED.fastq.gz \
+            --readFilesCommand zcat --outFileNamePrefix "$ANALYSIS"/ALIGN/"$i"_ --outSAMtype BAM Unsorted --outReadsUnmapped Fastx
         done
     fi
 
-#    if [[ "$LEVEL" == 6 ]]; then
-
+    if [[ "$LEVEL" == 6 ]]; then
+        featureCounts -a "$ANALYSIS"/REFERENCE/gencode.v38.chr_patch_hapl_scaff.annotation.gtf -o counts_CHIKV_CTRL.txt \
+        -s 2 -G "$ANALYSIS"/REFERENCE/GRCh38.p13.genome.fa -p --countReadPairs -C -T "$THREADS" \
+        $(cat "$SAMPLESHEET" | awk -F, -v ALIGN="$ANALYSIS"/ALIGN/ '$2~/CHIKV|CTRL/ {print ALIGN $1"_Aligned.out.bam"}' | xargs)
+        featureCounts -a "$ANALYSIS"/REFERENCE/gencode.v38.chr_patch_hapl_scaff.annotation.gtf -o counts_CHIKV_DENV.txt \
+        -s 2 -G "$ANALYSIS"/REFERENCE/GRCh38.p13.genome.fa -p --countReadPairs -C -T "$THREADS" \
+        $(cat "$SAMPLESHEET" | awk -F, -v ALIGN="$ANALYSIS"/ALIGN/ '$2~/CHIKV|DENV/ {print ALIGN $1"_Aligned.out.bam"}' | xargs)
+        featureCounts -a "$ANALYSIS"/REFERENCE/gencode.v38.chr_patch_hapl_scaff.annotation.gtf -o counts_CHIKV_ZIKV.txt \
+        -s 2 -G "$ANALYSIS"/REFERENCE/GRCh38.p13.genome.fa -p --countReadPairs -C -T "$THREADS" \
+        $(cat "$SAMPLESHEET" | awk -F, -v ALIGN="$ANALYSIS"/ALIGN/ '$2~/CHIKV|ZIKV/ {print ALIGN $1"_Aligned.out.bam"}' | xargs)
+        featureCounts -a "$ANALYSIS"/REFERENCE/gencode.v38.chr_patch_hapl_scaff.annotation.gtf -o counts_DENV_CTRL.txt \
+        -s 2 -G "$ANALYSIS"/REFERENCE/GRCh38.p13.genome.fa -p --countReadPairs -C -T "$THREADS" \
+        $(cat "$SAMPLESHEET" | awk -F, -v ALIGN="$ANALYSIS"/ALIGN/ '$2~/DENV|CTRL/ {print ALIGN $1"_Aligned.out.bam"}' | xargs)
+        featureCounts -a "$ANALYSIS"/REFERENCE/gencode.v38.chr_patch_hapl_scaff.annotation.gtf -o counts_DENV_ZIKV.txt \
+        -s 2 -G "$ANALYSIS"/REFERENCE/GRCh38.p13.genome.fa -p --countReadPairs -C -T "$THREADS" \
+        $(cat "$SAMPLESHEET" | awk -F, -v ALIGN="$ANALYSIS"/ALIGN/ '$2~/DENV|ZIKV/ {print ALIGN $1"_Aligned.out.bam"}' | xargs)
+        featureCounts -a "$ANALYSIS"/REFERENCE/gencode.v38.chr_patch_hapl_scaff.annotation.gtf -o counts_ZIKV_CTRL.txt \
+        -s 2 -G "$ANALYSIS"/REFERENCE/GRCh38.p13.genome.fa -p --countReadPairs -C -T "$THREADS" \
+        $(cat "$SAMPLESHEET" | awk -F, -v ALIGN="$ANALYSIS"/ALIGN/ '$2~/ZIKV|CTRL/ {print ALIGN $1"_Aligned.out.bam"}' | xargs)
+    fi
 
     end=$(date +%s.%N)
 
