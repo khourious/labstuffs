02a57c6011dd1f441b40bef1a5d6d0eaeb37aeac
Laise de Moraes <laise.moraes@fiocruz.br>
2022-12-01 18:45:01 -0300

    UpSet Plot

---
+++ Rscripts/UpSet.R
@@ -0,0 +1,37 @@
+# @lpmor22 | https://lpmor22.github.io/
+
+if (!requireNamespace("UpSetR", quietly = TRUE)) install.packages("UpSetR")
+if (!requireNamespace("BiocManager", quietly = TRUE)) install.packages("BiocManager")
+if (!requireNamespace("ComplexHeatmap", quietly = TRUE)) BiocManager::install("ComplexHeatmap")
+
+library("ComplexHeatmap")
+library("UpSetR")
+library("ggplot2")
+
+list <- read.csv("UpSet_Input.csv", check.names = FALSE)
+list
+
+matrix <- t(table(stack(list)))
+matrix <- t(list)
+matrix
+
+write.csv(matrix,"UpSet_Output1.csv", row.names = TRUE)
+
+pdf(file="UpSet_Output2.pdf")
+upset(fromList(list), 
+      nintersects = NA, 
+      # sets = c("SAMPLE_01", "SAMPLE_02", "SAMPLE_03", "SAMPLE_04", "SAMPLE_05"),
+      keep.order = TRUE,
+      # mainbar.y.label = "Intersection Size",
+      mainbar.y.max = 50,
+      # matrix.color = "#ff7b00",
+      main.bar.color = "#e71837",
+      sets.bar.color = "#27357e",
+      # sets.x.label = "Set Size",
+      point.size = 4.0,
+      line.size = 1.0,
+      # mb.ratio = c(0.6, 0.4),
+      order.by = "freq",
+      number.angles = 0,
+      text.scale = c(1, 1, 1, 1, 1, 1))
+dev.off()
