e1f709f64f866a15a2bf024eadcae35f1c9d0747
Laise de Moraes <laise.moraes@fiocruz.br>
2022-12-19 11:49:39 -0300

    UpSet Plot

--- Rscripts/UpSet/UpSet.R
+++ Rscripts/UpSet/UpSet.R
@@ -4,6 +4,8 @@ if (!requireNamespace("BiocManager", quietly = TRUE))
     install.packages("BiocManager", dependencies = TRUE)
 if (!requireNamespace("ComplexHeatmap", quietly = TRUE))
     BiocManager::install("ComplexHeatmap", dependencies = TRUE)
+if (!requireNamespace("ggplot2", quietly = TRUE))
+    install.packages("ggplot2", dependencies = TRUE)
 if (!requireNamespace("rstudioapi", quietly = TRUE))
     install.packages("rstudioapi", dependencies = TRUE)
 if (!requireNamespace("UpSetR", quietly = TRUE))
@@ -25,23 +27,17 @@ matrix <- t(table(stack(list)))
 matrix <- t(matrix[, -1])
 matrix
 
-write.csv(matrix, "UpSet_Output1.csv", row.names = TRUE)
+write.csv(matrix, "UpSet_Output.csv", row.names = TRUE)
 
-pdf(file = "UpSet_Output2.pdf")
-upset(fromList(list), 
-      nintersects = NA, 
+svg("UpSet_Output.svg")
+upset(fromList(list), nintersects = NA, keep.order = TRUE, point.size = 4.0, line.size = 1.0,
       # sets = c("SAMPLE_01", "SAMPLE_02", "SAMPLE_03", "SAMPLE_04", "SAMPLE_05"),
-      keep.order = TRUE,
       # mainbar.y.label = "Intersection Size",
       mainbar.y.max = 50,
       # matrix.color = "#ff7b00",
       main.bar.color = "#e71837",
       sets.bar.color = "#27357e",
       # sets.x.label = "Set Size",
-      point.size = 4.0,
-      line.size = 1.0,
       # mb.ratio = c(0.6, 0.4),
-      order.by = "freq",
-      number.angles = 0,
-      text.scale = c(1, 1, 1, 1, 1, 1))
+      order.by = "freq", number.angles = 0, text.scale = c(1, 1, 1, 1, 1, 1))
 dev.off()
