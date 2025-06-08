bb82c7bac2acce1a58709dce49786989e76f3cbf
Laise de Moraes <laise.moraes@fiocruz.br>
2025-06-08 15:34:47 -0300

    Gantt Chart

--- scripts/R/ganttChart/ganttChart.r
+++ scripts/R/ganttChart/ganttChart.r
@@ -1,59 +1,110 @@
-# https://lpmor22.github.io/ | 2025-06-03
+# https://lpmor22.github.io/ | 2025-06-08
 
 if (!requireNamespace("pacman", quietly = TRUE))
   install.packages("pacman", dependencies = TRUE)
 library("pacman")
 
-p_load(cowplot, ggplot2, this.path, tidyr)
+p_load(cowplot, dplyr, ggplot2, this.path, )
 
 setwd(dirname(this.path()))
 
 input <- "KhouriousTeam2025.csv"
+# Dhara Isabella Silva,Undergraduated student,2020.01.01,YYYY.MM.DD
+# Laura Barreto,Undergraduated student,2020.01.01,YYYY.MM.DD
 
 df <- read.csv(input, header = TRUE)
 
-acts <- c("Ricardo Khouri", "Valdomiro Moitinho", "Maria da Purificacao",
-          "Laise de Moraes", "Lia Barbara Arruda", "Cibele Orge",
-          "Luciane Amorim", "Lucas Campos", "Camila Brito", "Felipe Torres",
-          "Lucas Gentil","Lais Cambui", "Marcos Braz", "Isabele Carvalho",
-          "Thaline Mabel Santos", "Victoria Ribeiro", "Kleverton Ribeiro",
-          "Debora Magnavita", "Jessica Duarte", "Raphael Borges", "Marina Cucco",
-          "Ellen Pimentel", "Luisa Pedrosa", "Lilian Gomes", "Raphaela Andrade",
-          "Jose Adriano Silva", "Agatha Morais", "Ana Carolina Lima",
-          "Beatriz Vasconcelos", "Giulia Botelho", "Igor Silveira",
-          "Joyce Silva", "Icaro Strobel", "Clara Porto", "Dhara Isabella Silva",
-          "Laura Barreto", "Jessica Lais Santos", "Caio Galiano"
+order <- unique(df$name)
+df$name <- factor(df$name, levels = rev(order))
+
+colors <- c(
+  "PI" = "#e97a7f",
+  "Research Associate" = "#ec90e1",
+  "Postdoctoral Researcher" = "#b693ed",
+  "Ph.D. Student" = "#8db5cd",
+  "M.Sc. Student" = "#acd3ec",
+  "Undergraduate Student" = "#98ebae"
 )
 
-els <- c("PI", "Research Associate", "Ph.D. student", "pre-Ph.D. student",
-         "M.Sc. student", "pre-M.Sc. student", "Undergraduated student",
-         "Technician"
-)
+df <- df %>% mutate(color = colors[type])
+df$type <- factor(df$type, levels = names(colors))
 
-actcols <- c("#FA4749", "#EF8CC8", "#79E16F", "#39A638", "#2C872D",
-             "#19ADFF", "#193BFF", "#907AFF")
-
-gantt <- gather(df, "state", "date", 4:5) %>%
-    mutate(date = as.Date(date, "%Y.%m.%d"),
-           a2 = factor(a2, acts[length(acts):1]),
-           a3 = factor(a3, els))
-
-gantt_chart <- ggplot(gantt, aes(date, a2, colour = a3)) +
-  geom_line(linewidth = 3) +
-  scale_color_manual(values = actcols) +
+chart <- ggplot(df,
+                aes(x = as.Date(start, "%Y.%m.%d"),
+                    xend = as.Date(end, "%Y.%m.%d"),
+                    y = name,
+                    yend = name,
+                    color = type)) +
+  geom_segment(linewidth = 6) +
+  scale_color_manual(
+    values = colors,
+    breaks = names(colors)) +
+  scale_x_date(
+    expand = expansion(mult = c(0, 0)),
+    date_breaks = "1 year",
+    date_labels = "%Y") +
   theme_classic() +
-  scale_x_date(limits = c(as.Date("2015-01-01"), NA),
-               expand = c(0, 0), breaks = "1 year", date_labels = "%Y") +
-  theme(panel.grid.major.y = element_blank(),
-        panel.grid.major.x = element_line(linetype = "dashed", size = .5),
-        panel.grid.minor = element_line(linetype = "dashed", size = .5),
-        axis.line = element_line(colour = "#000000"),
-        legend.position = c(.21, .3),
-        legend.title = element_blank(),
-        legend.background = element_rect(size = .5,
-                                         linetype = "solid",
-                                         colour = "#BEBEBE")) +
+  theme(
+    panel.grid.major.x = element_line(linetype = "solid", linewidth = 1),
+    panel.grid.minor = element_line(linetype = "solid", linewidth = .5),
+    legend.position = c(.15,.2),
+    legend.title = element_blank(),
+    legend.background = element_rect(color = "#BEBEBE", linewidth = .5)) +
   labs(x = NULL, y = NULL)
+chart
 
-save_plot(sub("\\.csv$", ".pdf", input), gantt_chart, base_height = 5, base_width = 10)
-save_plot(sub("\\.csv$", ".png", input), gantt_chart, base_height = 5, base_width = 10)
+save_plot(sub("\\.csv$", ".pdf", input), chart, base_height = 10, base_width = 12)
+save_plot(sub("\\.csv$", ".png", input), chart, base_height = 10, base_width = 12)
+
+# engaging team members
+
+excl <- c(
+  "Valdomiro Moitinho",
+  "Maria da Purificação",
+  "Lia Barbara Arruda",
+  "Cibele Orge",
+  "Lucas Campos",
+  "Camila Brito",
+  "Lucas Gentil",
+  "Felipe Torres",
+  "Kleverton Ribeiro",
+  "Victoria Ribeiro",
+  "Laís Cambuí",
+  "Marina Cucco",
+  "Raphaela Andrade",
+  "Ellen Pimentel",
+  "Agatha Morais",
+  "Igor Silveira",
+  "Giulia Botelho",
+  "Ícaro Ströbel",
+  "Jessica Lais Santos"
+)
+
+df <- df %>% filter(!name %in% excl)
+
+chart <- ggplot(df,
+                aes(x = as.Date(start, "%Y.%m.%d"),
+                    xend = as.Date(end, "%Y.%m.%d"),
+                    y = name,
+                    yend = name,
+                    color = type)) +
+  geom_segment(linewidth = 6) +
+  scale_color_manual(
+    values = colors,
+    breaks = names(colors)) +
+  scale_x_date(
+    expand = expansion(mult = c(0, 0)),
+    date_breaks = "1 year",
+    date_labels = "%Y") +
+  theme_classic() +
+  theme(
+    panel.grid.major.x = element_line(linetype = "solid", linewidth = 1),
+    panel.grid.minor = element_line(linetype = "solid", linewidth = .5),
+    legend.position = c(.15,.2),
+    legend.title = element_blank(),
+    legend.background = element_rect(color = "#BEBEBE", linewidth = .5)) +
+  labs(x = NULL, y = NULL)
+chart
+
+save_plot(sub("\\.csv$", "etm.pdf", input), chart, base_height = 6.1, base_width = 12)
+save_plot(sub("\\.csv$", "etm.png", input), chart, base_height = 6.1, base_width = 12)
